cbe7de84e18337b7286eeb4ab73ffaf60048f3b2
geppetto <ad105geppetto@gmail.com>
2022-05-11 22:14:55 +0900

    update Login component

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,17 @@
 function Login() {
   return (
     <div className="Login">
-      <h1>Login page</h1>
+      <div className="inputField">
+        <div>Username</div>
+        <input name="userId" />
+      </div>
+      <div className="inputField">
+        <div>Password</div>
+        <input name="password" />
+      </div>
+      <div className="loginBtnContainer">
+        <button className="loginBtn">JWT Login</button>
+      </div>
     </div>
   );
 }
