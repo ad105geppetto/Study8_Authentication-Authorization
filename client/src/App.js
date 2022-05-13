1e37e361ab1526156f38df6415c205a28dfd6de5
geppetto <ad105geppetto@gmail.com>
2022-05-13 21:48:59 +0900

    [fix] Log-In component

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import Login from "./components/Login";
 import Mypage from "./components/Mypage";
 
 function App() {
-  const [isLogin, setIsLogin] = useState(true);
+  const [isLogin, setIsLogin] = useState(false);
   return (
     <div className="App">
       {isLogin ? (
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,16 +1,53 @@
+import { useState } from "react";
+
+const axios = require("axios");
+
 function Login() {
+  const [userId, setUserId] = useState("");
+  const [password, setPassword] = useState("");
+
+  const inputUserIdHandler = (e) => {
+    setUserId(e.target.value);
+  };
+  const inputPasswordHandler = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const loginRequestHandler = () => {
+    axios
+      .post("https://localhost:4000/login", {
+        userId: userId,
+        password: password,
+      })
+      .then((res) => {
+        console.log("ok");
+      });
+  };
+
   return (
     <div className="Login">
       <div className="inputField">
         <div>Username</div>
-        <input name="userId" />
+        <input
+          name="userId"
+          value={userId}
+          onChange={inputUserIdHandler}
+          type="text"
+        />
       </div>
       <div className="inputField">
         <div>Password</div>
-        <input name="password" />
+        <input
+          name="password"
+          value={password}
+          onChange={inputPasswordHandler}
+          type="password"
+        />
       </div>
       <div className="loginBtnContainer">
-        <button className="loginBtn">JWT Login</button>
+        <button className="loginBtn" onClick={loginRequestHandler}>
+          JWT Login
+        </button>
       </div>
     </div>
   );
