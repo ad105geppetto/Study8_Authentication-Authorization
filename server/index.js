23a7bbacb4dd715d8bcd22814eab56acc5b7fc17
geppetto <ad105geppetto@gmail.com>
2022-05-14 22:59:03 +0900

    [fix] update https protocol

--- server/index.js
+++ server/index.js
@@ -1,11 +1,36 @@
+const fs = require("fs");
+const https = require("https");
 const express = require("express");
-const server = express();
-const PORT = 4000;
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const app = express();
+const HTTPS_PORT = 4000;
 
-server.use("/", (req, res) => {
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  cors({
+    origin: ["https://localhost:3000"],
+    credentials: true,
+    methods: ["GET", "POST", "OPTIONS"],
+  })
+);
+app.use(cookieParser());
+
+app.use("/", (req, res) => {
   res.send("hello");
 });
 
-server.listen(PORT, () => {
-  console.log(`Example server listening on port http://localhost:${PORT}`);
-});
+let server;
+if (fs.existsSync("./key.pem") && fs.existsSync("./cert.pem")) {
+  const privateKey = fs.readFileSync(__dirname + "/key.pem", "utf8");
+  const certificate = fs.readFileSync(__dirname + "/cert.pem", "utf8");
+  const credentials = { key: privateKey, cert: certificate };
+
+  server = https.createServer(credentials, app);
+  server.listen(HTTPS_PORT, () =>
+    console.log("server runnning https://localhost:4000")
+  );
+} else {
+  server = app.listen(HTTPS_PORT);
+}
