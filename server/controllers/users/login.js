c68f1032c470bff60480a5d56ab6f66308424cb7
sharebox-백 <ad105geppetto@gmail.com>
2022-05-22 23:55:43 +0900

    [fix]server/controllers/users/login

--- server/controllers/users/login.js
+++ server/controllers/users/login.js
@@ -1,4 +1,11 @@
-module.exports = (req, res) => {
-  console.log("dfd");
+const jwt = require("jsonwebtoken");
+const { Users } = require("../../models");
+
+module.exports = async (req, res) => {
+  const userInfo = await Users.findOne({
+    where: { userId: req.body.userId, password: req.body.password },
+  });
+
+  console.log(userInfo);
   res.send("ok");
 };
