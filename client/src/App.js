9250148ef094bd7717884f5eaaadca48a75d448a
sharebox-백 <ad105geppetto@gmail.com>
2022-05-30 02:44:45 +0900

    [feat] add redirection git->client

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Login from "./components/Login";
 import Mypage from "./components/Mypage";
 
 function App() {
   const [isLogin, setIsLogin] = useState(false);
   const [accessToken, setAccessToken] = useState("");
+
+  const getAccessToken = async (authorizationCode) => {
+    let resp = await axios.post("http://localhost:4000/callback", {
+      authorizationCode: authorizationCode,
+    });
+    setIsLogin(true);
+    setAccessToken(resp.data.accessToken);
+  };
+
+  useEffect(() => {
+    const url = new URL(window.location.href);
+    const authorizationCode = url.searchParams.get("code");
+    if (authorizationCode) {
+      getAccessToken(authorizationCode);
+    }
+  }, []);
 
   const loginHandler = (data) => {
     console.log(data);
